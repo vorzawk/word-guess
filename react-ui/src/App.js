e44b6fe8aeeb01d3f83b68164abd3d0c4f1ba849
vorzawk <21965736+vorzawk@users.noreply.github.com>
2020-01-25 19:17:24 +0530

    basic static version of the website

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -2,14 +2,33 @@ import React from "react";
 import data from "./data.json";
 import Card from "./Card";
 
-const App = () => {
-  return (
-    <div className="App">
-      <h1> Guess the words based on the prompts </h1>
-      <Card prompt={data[0]["definition"]} />
-      <Card prompt={data[1]["definition"]} />
-    </div>
-  );
-};
+class App extends React.Component {
+  renderCard(i) {
+    return (
+      <Card
+        index={i}
+        prompt={data[i]["definition"]}
+        example={data[i]["example"]}
+        word={data[i]["word"]}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Guess the words </h1>
+        <hr />
+        <div className="card-grid-container">
+          <div className="card-grid">
+            {this.renderCard(0)} {this.renderCard(1)} {this.renderCard(2)}
+            {this.renderCard(3)} {this.renderCard(4)} {this.renderCard(5)}
+            {this.renderCard(6)} {this.renderCard(7)} {this.renderCard(8)}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
--- react-ui/src/Card.js
+++ react-ui/src/Card.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 const Card = props => {
+  const clue = props.example.replace(props.word, "______");
   return (
-    <div>
-      <h3> Word 1 </h3>
-      <p> {props.prompt} </p>
+    <div className="card">
+      <p>Guess one word for </p>
+      <p className="prompt">
+        <em>{props.prompt}</em>
+      </p>
+      <p className="clue"> {clue} </p>
+      <div>
+        <form>
+          <input id="response" type="text" name="response" />
+          <input type="button" value="Submit" />
+        </form>
+      </div>
     </div>
   );
 };
