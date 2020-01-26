5f04c21d76d4eba6c85f98dbde1a91d1ed0752e9
vorzawk <21965736+vorzawk@users.noreply.github.com>
2020-01-26 22:57:35 +0530

    code refactored

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -4,14 +4,8 @@ import Card from "./Card";
 
 class App extends React.Component {
   renderCard(i) {
-    return (
-      <Card
-        index={i}
-        prompt={data[i]["definition"]}
-        example={data[i]["example"]}
-        word={data[i]["word"]}
-      />
-    );
+    let word = data[i];
+    return <Card prompt={word["definition"]} example={word["example"]} />;
   }
 
   render() {
@@ -19,12 +13,16 @@ class App extends React.Component {
       <div className="App">
         <h1> Guess the words </h1>
         <hr />
-        <div className="card-grid-container">
-          <div className="card-grid">
-            {this.renderCard(0)} {this.renderCard(1)} {this.renderCard(2)}
-            {this.renderCard(3)} {this.renderCard(4)} {this.renderCard(5)}
-            {this.renderCard(6)} {this.renderCard(7)} {this.renderCard(8)}
-          </div>
+        <div className="card-grid">
+          {this.renderCard(0)}
+          {this.renderCard(1)}
+          {this.renderCard(2)}
+          {this.renderCard(3)}
+          {this.renderCard(4)}
+          {this.renderCard(5)}
+          {this.renderCard(6)}
+          {this.renderCard(7)}
+          {this.renderCard(8)}
         </div>
       </div>
     );
--- react-ui/src/Card.js
+++ react-ui/src/Card.js
@@ -1,20 +1,37 @@
 import React from "react";
 
+const Prompt = props => {
+  return (
+    <p className="prompt">
+      <em>{props.prompt}</em>
+    </p>
+  );
+};
+
+const Clue = props => {
+  let clue = props.example.replace(props.word, "______");
+  console.log(clue);
+  return <p className="clue"> {clue} </p>;
+};
+
+const Form = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" name="response" />
+        <input type="button" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
 const Card = props => {
-  const clue = props.example.replace(props.word, "______");
   return (
     <div className="card">
       <p>Guess one word for </p>
-      <p className="prompt">
-        <em>{props.prompt}</em>
-      </p>
-      <p className="clue"> {clue} </p>
-      <div>
-        <form>
-          <input id="response" type="text" name="response" />
-          <input type="button" value="Submit" />
-        </form>
-      </div>
+      <Prompt prompt={props.prompt} />
+      <Clue example={props.example} />
+      <Form />
     </div>
   );
 };
