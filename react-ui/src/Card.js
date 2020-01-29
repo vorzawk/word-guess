17e4ae63949e357602fad9d62e5e1bc8407321bc
vorzawk <21965736+vorzawk@users.noreply.github.com>
2020-01-29 17:19:11 +0530

    mvp completed!

--- react-ui/src/Card.js
+++ react-ui/src/Card.js
@@ -1,4 +1,5 @@
 import React from "react";
+import UserInteraction from "./UserInteraction";
 
 const Prompt = props => {
   return (
@@ -25,99 +26,6 @@ class Clue extends React.Component {
         onClick={() => this.setState({ isClicked: true })}
       >
         Click here for a clue
-      </div>
-    );
-  }
-}
-
-class Form extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "" };
-  }
-
-  handleChange = event => {
-    this.setState({ value: event.target.value });
-  };
-
-  onSubmit = () => {
-    const { value } = this.state;
-    this.props.handleSubmit(value);
-  };
-
-  render() {
-    return (
-      <form>
-        <input
-          type="text"
-          value={this.state.value}
-          onChange={this.handleChange}
-        />
-        <input type="button" value="Submit" onClick={this.onSubmit} />
-      </form>
-    );
-  }
-}
-class UserInteraction extends React.Component {
-  constructor(props) {
-    super(props);
-    this.initialState = {
-      isSubmitted: false,
-      isCorrect: false,
-      isAnsRequested: false
-    };
-    this.state = this.initialState;
-  }
-
-  handleSubmit = userResponse => {
-    const isCorrect = userResponse.toLowerCase() === this.props.wordStr;
-    const isSubmitted = true;
-    this.setState({ isSubmitted, isCorrect });
-  };
-
-  requestAnswer = () => {
-    this.setState({ isAnsRequested: true });
-  };
-
-  render() {
-    if (this.state.isAnsRequested) {
-      return (
-        <div className="userInteractions">
-          The correct answer is <strong>{this.props.wordStr}</strong>.
-        </div>
-      );
-    }
-    if (!this.state.isSubmitted) {
-      return (
-        <div className="userInteractions">
-          <Form handleSubmit={this.handleSubmit} />
-        </div>
-      );
-    }
-    if (this.state.isCorrect) {
-      return (
-        <div className="userInteractions">
-          Correct answer
-          <span role="img" aria-label="smiling face">
-            😄
-          </span>
-        </div>
-      );
-    }
-    return (
-      <div className="userInteractions">
-        <div>
-          Incorrect response
-          <span role="img" aria-label="slightly frowning face">
-            🙁
-          </span>
-        </div>
-        <div className="userOptions">
-          <button onClick={() => this.setState(this.initialState)}>
-            Try Again
-          </button>
-          <button onClick={this.requestAnswer}>Show answer</button>
-        </div>
       </div>
     );
   }
